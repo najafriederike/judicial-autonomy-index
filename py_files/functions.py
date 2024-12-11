7f9821d42d4abee99ce334e1614a188d6186772e
najafriederike <friederike.augustin@gmail.com>
2024-12-11 17:20:59 +0100

    Update Day3

--- py_files/functions.py
+++ py_files/functions.py
@@ -41,3 +41,105 @@ def cleaning_judicial_autonomy_data(qx_df):
     df_cleaned = df_countries_cleaned.drop(list(df_countries_cleaned.filter(regex='_subj')), axis=1)
 
     return df_cleaned
+
+
+
+def subset_dataset_into_indicators(qx_cleaned):
+
+    subsets = {
+
+    # Actors involved in appointment procedures of judges
+    'q1_appointment_actors': qx_cleaned[['country',
+                                        'year',
+                                        'q1c1_apjufc',
+                                        'q1c1_apjuac',
+                                        'q1c1_apjuhc',
+                                        'q1c1_appresidfc',
+                                        'q1c1_appresidac',
+                                        'q1c1_appresidhc',
+                                        'q1c1_exvetofc',
+                                        'q1c1_exvetoac',
+                                        'q1c1_exvetohc']],
+    
+    # Selection and appointment criteria for judges
+    'q1_appointment_criteria': qx_cleaned[['country',
+                                          'year',
+                                          'q1c1_critfclaw', 
+                                          'q1c1_critaclaw',
+                                          'q1c1_crithclaw', 
+                                          'q1c1_critfcints',
+                                          'q1c1_critacints', 
+                                          'q1c1_crithcints',
+                                          'q1c1_probju']],
+
+    # Transparency and mechanisms for appeal in appointment procedures of judges
+    'q1_appointment_transparency': qx_cleaned[['country',
+                                              'year',
+                                              'q1c1_transplaw', 
+                                              'q1c1_appealfc',
+                                              'q1c1_appealac',
+                                              'q1c1_appealhc']],
+    # Tenure and term in office of judges
+    'q1_judge_tenure': qx_cleaned[['country',
+                                  'year',
+                                  'q1c2_termfcju', 
+                                  'q1c2_termacju',
+                                  'q1c2_termpresid', 
+                                  'q1c2_termhcju',
+                                  'q1c2_retireage']],
+    
+    # Immunity and non-transferability of judges
+    'q1_judge_immunity': qx_cleaned[['country',
+                                    'year',
+                                    'q1c2_juabsimmun', 
+                                    'q1c2_jufuncimmun',
+                                    'q1c2_juremove', 
+                                    'q1c2_jutransf']],
+
+    # Salaries and bonuses of judges
+    'q1_judge_salary': qx_cleaned[['country',
+                                  'year',
+                                  'q1c2_jusalary', 
+                                  'q1c2_jubonus',
+                                  'q1c2_jupension']],
+        
+    # Disciplinary proceedings against judges
+    'q1_disciplinary_proceedings': qx_cleaned[['country',
+                                              'year',
+                                              'q1c3_fairtrial', 
+                                              'q1c3_disciplaw',
+                                              'q1c3_discipints', 
+                                              'q1c3_discipbody',
+                                              'q1c3_initdiscip', 
+                                              'q1c3_decdiscip',
+                                              'q1c3_immunlift', 
+                                              'q1c3_sanctscale',
+                                              'q1c3_appealdiscip']],
+        
+    # Conflict of interest, recusal from cases and evaluation of judges
+    'q1_conflict_recusal_evaluation': qx_cleaned[['country',
+                                          'year',
+                                          'q1c3_judisclos', 
+                                          'q1c3_jurestrict',
+                                          'q1c3_jurecuse']],
+                                          
+    # Composition and functioning of the judicial self-governing bodies
+    'q1_judicial_self_governance': qx_cleaned[['country',
+                                             'year',
+                                             'q1c4_whocharge', 
+                                             'q1c4_whoselect',
+                                             'q1c4_whochair', 
+                                             'q1c4_competence',
+                                             'q1c4_sameright', 
+                                             'q1c4_reasondecis']], 
+    
+    # Administration, functioning and budget of courts                                         
+    'q1_courts_administration': qx_cleaned[['country',
+                                           'year',
+                                           'q1c4_casealloc', 
+                                           'q1c4_regbudget',
+                                           'q1c4_manbudget']]
+    }
+
+    return subsets
+    
